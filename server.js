3b7cf903668461218f628be429986f4d4b63a1ac
ZeAfonso21 <joseafonsogat@gmail.com>
2025-07-01 20:50:31 +0200

    test to send hmtl page and js frontend code

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const https = require('https');
 const fs = require('fs');
 const express = require('express');
+import path from 'path';
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Secure Express server with Cloudflare Origin Cert');
+ // res.send('Secure Express server with Cloudflare Origin Cert');
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 const server = https.createServer({
